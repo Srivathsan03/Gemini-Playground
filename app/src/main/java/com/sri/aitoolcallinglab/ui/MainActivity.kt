8e857764d05e80ccaeeb39f3b5402fd76ef68c87
Srivathsan <srivathsanbe@gmail.com>
2026-07-06 11:00:10 +0530

    Reformat

--- app/src/main/java/com/sri/aitoolcallinglab/ui/MainActivity.kt
+++ app/src/main/java/com/sri/aitoolcallinglab/ui/MainActivity.kt
@@ -26,8 +26,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
-import com.sri.aitoolcallinglab.chat.ChatRunner
 import com.sri.aitoolcallinglab.R
+import com.sri.aitoolcallinglab.chat.ChatRunner
 import com.sri.aitoolcallinglab.ui.theme.AiToolCallingTheme
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
